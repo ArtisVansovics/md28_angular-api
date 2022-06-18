1c0b74b8bc304ca195ed1bae9c729b18fcc76f8f
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-18 20:22:51 +0300

    Created Characters and Locations pages and added them to routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { LocationsComponent } from './pages/locations/locations.component';
 
 @NgModule({
-  declarations: [AppComponent, ButtonComponent],
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    CharactersComponent,
+    LocationsComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
