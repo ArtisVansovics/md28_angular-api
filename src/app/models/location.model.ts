4c5b6426370e8cfd0b0420fc4f507abb3c1c7574
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-18 21:39:03 +0300

    Added models for character and location data

---
+++ src/app/models/location.model.ts
@@ -0,0 +1,16 @@
+import { Info } from './character.model';
+
+export interface LocationsData {
+  info: Info;
+  results: Location[];
+}
+
+export interface Location {
+  id: number;
+  name: string;
+  type: string;
+  dimension: string;
+  residents: string[];
+  url: string;
+  created: string;
+}
