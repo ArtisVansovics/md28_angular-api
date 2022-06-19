c178bd12108166d757d59b7e31ff9c129efd0731
Artis Vansovics <vansovics.artis@gmail.com>
2022-06-19 03:29:39 +0300

    Created CharacterForm to handle character filtering

---
+++ src/app/pages/characters/character-form/character-form.component.ts
@@ -0,0 +1,55 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CharacterQuery } from '../../../models/character.model';
+
+@Component({
+  selector: 'app-character-form',
+  templateUrl: './character-form.component.html',
+  styleUrls: ['./character-form.component.scss'],
+})
+export class CharacterFormComponent implements OnInit {
+  @Output() searchCharactersEvent = new EventEmitter<CharacterQuery>();
+
+  genderOptions = [
+    {
+      name: 'Female',
+      value: 'female',
+    },
+    {
+      name: 'Male',
+      value: 'male',
+    },
+    {
+      name: 'Genderless',
+      value: 'genderless',
+    },
+    {
+      name: 'Unknown',
+      value: 'unknown',
+    },
+  ];
+
+  charactersForm: FormGroup = this.fb.group({});
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.charactersForm = this.fb.group({
+      name: ['', [Validators.required]],
+      gender: '',
+    });
+  }
+
+  searchCharacters(): void {
+    if (this.charactersForm.valid) {
+      this.searchCharactersEvent.emit({
+        name: this.charactersForm.value.name,
+        gender: this.charactersForm.value.gender,
+      });
+    }
+  }
+}
